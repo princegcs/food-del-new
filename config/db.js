076ea41f847bc721f9ed8e1a436584adbd8e68a1
Prince <prirp21@gmail.com>
2024-05-08 09:45:48 +0530

    final project

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+    process.exit(1); // Exit process with failure
+  }
+};
---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import path from "path";
+
+const __dirname = path.resolve();
+
+// app config
+const app = express();
+const port = 4000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
+
+app.use(express.static(path.join(__dirname, "/admin/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/admin/dist/index.html"));
+});
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () =>
+  console.log(`Server started on http://localhost:${port}`)
+);
